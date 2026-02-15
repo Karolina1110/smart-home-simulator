78040de26892f665cb6a845369697aee719f5e11
Karolina1110 <karolinatujaka99@gmail.com>
2026-02-15 22:07:18 +0100

    added django rest framework

--- backend/django/apps/devices/urls.py
+++ backend/django/apps/devices/urls.py
@@ -1,6 +1,11 @@
-from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import DeviceViewSet
+
+router = DefaultRouter()
+router.register(r'', DeviceViewSet, basename='device')
 
 urlpatterns = [
-    
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls'))
 ]
